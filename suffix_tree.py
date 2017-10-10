0f4f75b7bfd5840d551de21b117ff4172c81eff7
Joar Gruneau <joar@gruneau.se>
2017-10-10 14:31:37 +0200

    wont work

--- suffix_tree.py
+++ suffix_tree.py
@@ -6,7 +6,6 @@ class Node(object):
         self.leaf_node = leaf_node
         self.parent = None
         self.children = {}
-        self.string_pointers = []
         self.start =None
         self.end = None
         self.suffix_link = None
@@ -17,16 +16,14 @@ class Node(object):
         else:
             return self.end - self.start +1
 
-    def get_pointer(self):
-        return self.string_pointers[0]
 
-    def add_pointer(self, pointer):
-        if len(self.string_pointers) == 0:
-            self.string_pointers.append(pointer)
-        else:
-            if(self.string_pointers[len(self.string_pointers) - 1] != pointer):
-                self.string_pointers.append(pointer)
-        print(self.string_pointers)
+    # def add_pointer(self, pointer):
+    #     if len(self.string_pointers) == 0:
+    #         self.string_pointers.append(pointer)
+    #     else:
+    #         if(self.string_pointers[len(self.string_pointers) - 1] != pointer):
+    #             self.string_pointers.append(pointer)
+    #     print(self.string_pointers)
 
     def __getattribute__(self, name):
         # print(name)
@@ -37,10 +34,18 @@ class Node(object):
 
 class Suffix_tree:
     def __init__(self, string_list):
-        self.string_list = string_list
+        self.string = ""
+        self.id_length = len(str(len(string_list)))+1
+        for i in range(len(string_list)):
+            self.string = self.string + string_list[i] + '#' + self.get_id(i) +'$' 
+        self.substring_length = len(string_list[0]) + self.id_length + 2 +3
+        print(self.string)
         self.root = None
 
         self.build_tree()
+
+    def get_id(self, number):
+        return '0'*(self.id_length - len(str(number))) + str(number)
 
     def reset(self):
         self.active_node = None
@@ -49,10 +54,9 @@ class Suffix_tree:
         self.remainder = 0
         self.last_created_node = None
         self.active_node = self.root
-        self.force_print_out = False
 
     def get_edge(self, node):
-        return self.string_list[node.string_pointers[0]][node.start: node.end + 1]
+        return self.string[node.start: node.end + 1]
 
     def create_node(self, start, end=None, parent = None, leaf_node = False):
         node = Node(leaf_node = leaf_node)
@@ -63,33 +67,19 @@ class Suffix_tree:
         return node
 
     def build_tree(self):
-        string_list_size = len(self.string_list)
+        size = len(self.string)
         self.root = self.create_node(start = -1, end = -1)
-        self.root.add_pointer(0)
-        for string_pointer in range(string_list_size):
-            self.reset()
-            size = len(self.string_list[string_pointer])
-            for index in range(size):
-                self.extend_tree(index, string_pointer)
+        self.reset()
+        global tree_depth
+        tree_depth = self.substring_length
+        for index in range(size):
+            if(self.string[index] == '$'):
+                self.reset()
+            else:
+                self.extend_tree(index)
 
 
 
-    def walk_down(self, current_node):
-        """Walk down from current node.
-
-        activePoint change for walk down (APCFWD) using
-        Skip/Count Trick  (Trick 1). If active_length is greater
-        than current edge length, set next  internal node as
-        active_node and adjust active_edge and active_length
-        accordingly to represent same activePoint.
-        """
-        length = current_node.length()
-        if (self.active_length >= length):
-            self.active_edge += length
-            self.active_length -= length
-            self.active_node = current_node
-            return True
-        return False
 
     # def force_print(self, index, string_pointer):
     #      while (self.remainder > 0):
@@ -112,33 +102,24 @@ class Suffix_tree:
     #             self.active_node = self.active_node.suffix_link
 
 
-    def extend_tree(self, index, string_pointer):
-        global tree_depth
-        tree_depth = index
-
+    def extend_tree(self, index):
         self.remainder += 1
         self.last_created_node = None
-        force_printout = False
-        print(self.string_list[string_pointer])
+
 
         while (self.remainder > 0):
-            end_node = self.active_node.children.get('$')
-            if(end_node != None):
-                end_node.add_pointer(string_pointer)
-
             if (self.active_length == 0):
                 self.active_edge = index
 
-            if (self.active_node.children.get(self.string_list[string_pointer][self.active_edge]) is None):
+            if (self.active_node.children.get(self.string[self.active_edge]) is None):
                 new_node = self.create_node(index, leaf_node=True)
-                new_node.add_pointer(string_pointer)
 
-                self.active_node.children[self.string_list[string_pointer][self.active_edge]] = new_node
+                self.active_node.children[self.string[self.active_edge]] = new_node
                 if (self.last_created_node is not None):
                     self.last_created_node.suffix_link = self.active_node
                     self.last_created_node = None
             else:
-                child = self.active_node.children.get(self.string_list[string_pointer][self.active_edge])
+                child = self.active_node.children.get(self.string[self.active_edge])
 
                 # if(self.string_list[string_pointer][index] =='$'):
                 #     child.add_pointer(string_pointer)
@@ -150,26 +131,22 @@ class Suffix_tree:
                     self.active_node = child
                     continue
 
-                if (self.string_list[child.get_pointer()][child.start + self.active_length] == self.string_list[string_pointer][index]):
+                if (self.string[child.start + self.active_length] == self.string[index]):
                     if((self.last_created_node is not None) and (self.active_node != self.root)):
                         self.last_created_node.suffix_link = self.active_node
                         self.last_created_node  = None
 
                     self.active_length += 1
-                    # if self.string_list[string_pointer][index] == '$':
-                    #     self.force_print(index, string_pointer)
                     break
 
                 split_end = child.start + self.active_length - 1
                 split = self.create_node(start = child.start,  end =split_end)
-                split.add_pointer(child.get_pointer())
-                self.active_node.children[self.string_list[child.get_pointer()][self.active_edge]] = split
+                self.active_node.children[self.string[self.active_edge]] = split
 
                 new_node = self.create_node(index, leaf_node=True)
-                new_node.add_pointer(string_pointer)
-                split.children[self.string_list[string_pointer][index]] = new_node
+                split.children[self.string] = new_node
                 child.start += self.active_length
-                split.children[self.string_list[child.string_pointers[0]][child.start]] = child
+                split.children[self.string[child.start]] = child
 
                 if (self.last_created_node is not None):
                     self.last_created_node.suffix_link = split
@@ -184,7 +161,7 @@ class Suffix_tree:
                 self.active_node = self.active_node.suffix_link
 
     def print_clear(self, spacer, node):
-        print(spacer + "node = " + self.get_edge(node) + " str_pointers " + str(node.string_pointers))
+        print(spacer + "node = " + self.get_edge(node))
         spacer = spacer + "  |  "
         for _, child in node.children.items():
             self.print_clear(spacer, child)
@@ -245,7 +222,7 @@ class Suffix_tree:
 
 
 if __name__ =="__main__":
-    suf = Suffix_tree(['TGGA$', 'TGGSA$'])
+    suf = Suffix_tree(['TGGA', 'TGSA'])
     suf.print_clear("", suf.root)
     # print(suf.longest_suffix("TGGAATTCTCGGGTGCCAAGGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG"))
 
